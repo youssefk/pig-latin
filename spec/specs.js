cea97f59e454ccbb77ec082996c13893a2297080
Youssef & Nathan <Guest@epicodus-28.local>
2015-10-21 18:42:33 -0700

    Fix first spec to match expected output.

--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,8 @@
 describe('pigLatin', function() {
   it("adds 'ay' to the end of all words starting with a vowel", function() {
-    expect(pigLatin("apple")).to.equal(true);
+    expect(pigLatin("apple")).to.equal("appleay");
   });
-  it("takes all words that start with one or more consonants, move all of the first consecutive consonants to the end and add 'ay'", function() {
+  /*it("takes all words that start with one or more consonants, move all of the first consecutive consonants to the end and add 'ay'", function() {
     expect(pigLatin("tree")).to.equal(true);
   });
   it("takes all 'qu' words and moves the qu to the end together", function() {
@@ -10,5 +10,5 @@ describe('pigLatin', function() {
   });
   it("treats the letter y as a consonant for words that start with the letter", function() {
     expect(pigLatin("yesterday")).to.equal(true);
-  });
+  });*/
 });
